41814c356f7b31807f92f7df728a095ab5ba8f63
dmachnia <01178558@pw.edu.pl>
2026-07-18 22:31:46 +0200

    Add initial project structure with .gitignore, main.py, pyproject.toml, and setup.py

--- setup.py
+++ setup.py
@@ -1,30 +1,3 @@
-import os
-from setuptools import setup, find_packages
-import pkg_resources
+from setuptools import find_packages, setup
 
-# Path to the requirements file
-requirements_path = os.path.join(os.path.dirname(__file__), "requirements.txt")
-
-# Read the requirements from the requirements file
-if os.path.exists(requirements_path):
-    with open(requirements_path, 'r') as f:
-        install_requires = [str(r) for r in pkg_resources.parse_requirements(f)]
-else:
-    install_requires = []
-
-setup(
-    name="diffsynth",
-    version="1.1.7",
-    description="Enjoy the magic of Diffusion models!",
-    author="Artiprocher",
-    packages=find_packages(),
-    install_requires=install_requires,
-    include_package_data=True,
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: Apache Software License",
-        "Operating System :: OS Independent",
-    ],
-    package_data={"diffsynth": ["tokenizer_configs/**/**/*.*"]},
-    python_requires='>=3.6',
-)
+setup(name="flashvsr", version="0.1", packages=find_packages(), author="Daniel Machniak")
